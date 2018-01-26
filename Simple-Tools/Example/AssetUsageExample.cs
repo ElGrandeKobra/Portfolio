792f12e37708382b7388b8f7ca3b6e62f7a38a23
Miles Every <milesevery2000@gmail.com>
2018-01-25 21:24:53 -0500

    Simple Tools

---
+++ Simple-Tools/Example/AssetUsageExample.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleTools;
+
+public class AssetUsageExample : MonoBehaviour {
+	public WeaponAsset[] weapons;
+
+	void Start(){
+		foreach (WeaponAsset weapon in weapons) {
+			Debug.Log (weapon.name + " - " + weapon.damage + " damage");
+		}
+	}
+}
