a272f405228dbc05803b1e602b9751a006d6dd1f
Miles Every <milesevery1234556@aol.com>
2019-01-24 19:05:32 -0500

    Dynamic, Multithreaded LOD/Culling System

---
+++ MultithreadedGraphics/Assets/ThreadedObjectInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThreadedObjectInfo : MonoBehaviour {
+    public Mesh[] meshes;
+    public Material[] materials;
+    public float[] renderDistances;
+    public float[] qualitySettingMultipliers = { .1f, .2f, .4f, .6f, .8f, 1 };
+}
---
+++ MultithreadedGraphics/Assets/ThreadedObstacle.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThreadedObstacle : MonoBehaviour {
+    public float radius;
+    /// <summary>
+    /// Set to 0 for inner obstacles and a higher number like .6 for outer ones
+    /// </summary>
+    [Range(0,3)]
+    public float offset = .6f;
+    public Vector3 pos;
+    Transform trans;
+    int index = -1;
+    public Vector3 dir;
+    public Vector3 dirNorm;
+    public float sumRad;
+    public float dist;
+    public float cosAngle;
+
+	private void updatePosition()
+	{
+        pos = trans.position;
+	}
+
+	private void OnEnable()
+	{
+        trans = transform;
+        if(ThreadedLODS.instance){
+            bool found = false;
+            for (int i = 0; i < ThreadedLODS.instance.threadedObstacles.Count;i++){
+                if(ThreadedLODS.instance.threadedObstacles[i] == null){
+                    index = i;
+                    ThreadedLODS.instance.threadedObstacles[i] = this;
+                    found = true;
+                    continue;
+                }
+            }
+            if(!found){
+                  ThreadedLODS.instance.threadedObstacles.Add(this);
+            }
+        }else{
+            Invoke("OnEnable", 1);
+        }
+        updatePosition();
+	}
+
+	private void OnDestroy()
+	{
+        if (ThreadedLODS.instance)
+        {
+            ThreadedLODS.instance.threadedObstacles[ThreadedLODS.instance.threadedObstacles.IndexOf(this)] = null;
+        }
+	}
+	private void OnDrawGizmos()
+	{
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radius);
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, radius + offset + .01f);
+	}
+}
